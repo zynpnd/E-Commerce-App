91dbcd97fff354c910ec272e73293900d74da731
ZEYNEP NİDA SARICAN <56023452+zynpnd@users.noreply.github.com>
2024-01-26 11:32:34 +0300

    Shopping App Frontend
    Navbar, Slider, Categories,  Product List UI, Newsletter, Footer design.
    Category, single product page, login and register, shopping cart page design

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
+import Cart from './pages/Cart';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Product from './pages/Product';
+import ProductList from './pages/ProductList';
+import Register from './pages/Register';
 
 function App() {
   return (
-      <Home/>
+      <Home />
   );
 }
 
---
+++ client/src/data.js
@@ -0,0 +1,95 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://i.ibb.co/cXFnLLV/3.png",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/DG69bQ4/2.png",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/cXFnLLV/3.png",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      title: "LIGHT JACKETS",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:3,
+      img:"https://www.vintageindustries.nl/download_front/skfbmd2111/3546_Gray_T_shirt_Ocean_Blue.png",
+    },
+    {
+      id:4,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:5,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:6,
+      img:"https://www.vintageindustries.nl/download_front/ikrpag1981/2208_Datton_Jacket_Dark_Olive.png",
+    },
+    {
+      id:7,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+    {
+      id:8,
+       img:"https://www.vintageindustries.nl/download_front/gdyjao2057/1040_Baxter_jogger_birch.png",
+    },
+    {
+      id:9,
+      img:"https://www.vintageindustries.nl/download_front/wkcofp2265/30124_Caldwell_jacket_Black_Front.png",
+    },
+    {
+      id:10,
+      img:"https://www.pngarts.com/files/1/Purse-Transparent-Background-PNG.png",
+    },
+    {
+      id:11,
+      img:"https://www.pngarts.com/files/2/Black-Winter-Jacket-For-Women-PNG-Image-With-Transparent-Background.png",
+    },
+    {
+      id:12,
+      img:"https://www.pngarts.com/files/10/Earring-PNG-Pic.png",
+    },
+
+
+   
+  ]
